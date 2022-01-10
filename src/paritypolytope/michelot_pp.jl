44d93affcbcde9545e2e4daa3182d13c31fda305
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2022-01-09 19:40:07 -0500

    Add files via upload

---
+++ src/paritypolytope/michelot_pp.jl
@@ -0,0 +1,53 @@
+#michelot_pp.jl
+using ThreadsX
+using Base.Threads
+
+function serial_michelot_pp(data::AbstractVector)::AbstractVector
+    let
+        f=Array{AbstractFloat}(undef, length(data))
+        for i in 1:length(data); f[i] = data[i] < 0 ? 0 : 1; end
+        if sum(f)%2 == 0
+            i = findmin(abs.(data))[2]
+            f[i] = 1 - f[i]
+        end
+        v=Array{AbstractFloat}(undef, length(data))
+        d=Array{AbstractFloat}(undef, length(data))
+        for i in 1:length(data)
+            v[i] = data[i]*(-1)^(f[i])
+            d[i] = max(min(v[i], 0.5), -0.5)
+        end
+        if sum(d) >= (1-length(data)/2)
+            for i in 1:length(data); d[i] = max(min(data[i], 0.5), -0.5); end
+            return d
+        else
+            x=michelot_s(v,1)
+            for i in 1:length(data); x[i]=(x[i]-0.5)*(-1)^(f[i]); end
+            return x
+        end
+    end
+end
+
+function parallel_michelot_pp(data::AbstractVector)::AbstractVector
+    let
+        f=Array{AbstractFloat}(undef, length(data))
+        @threads for i in 1:length(data); @inbounds f[i] = data[i] < 0 ? 0 : 1; end
+        if sum(f)%2 == 0
+            i = findmin(abs.(data))[2]
+            f[i] = 1 - f[i]
+        end
+        v=Array{AbstractFloat}(undef, length(data))
+        d=Array{AbstractFloat}(undef, length(data))
+        @threads for i in 1:length(data)
+            @inbounds v[i] = data[i]*(-1)^(f[i])
+            @inbounds d[i] = max(min(v[i], 0.5), -0.5)
+        end
+        if ThreadsX.sum(d)>=(1-length(data)/2)
+            @threads for i in 1:length(data); @inbounds d[i] = max(min(data[i], 0.5), -0.5); end
+            return d
+        else
+            x=michelot_p(v,1)
+            @threads for i in 1:length(data); @inbounds x[i]=(x[i]-0.5)*(-1)^(f[i]); end
+            return x
+        end
+    end
+end
